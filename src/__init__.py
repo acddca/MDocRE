d972fda21a797361490d34c2dfae3457211e6211
jake <klxstar@126.com>
2024-04-07 08:07:59 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,16 +5,6 @@
 # @Email: wangjl.nju.2020@gmail.com.
 from .mdocre_model import MDocREModel
 from .mdocre_trainer import MDocRETrainer
-from .bert_model import EnhancedBert
-from .bert_trainer import BertTrainer
-from .atlop_model import ATLOPModel
-from .atlop_trainer import ATLOPTrainer
-from .maebert_base_model import MaeBertBaseModel
-from .maebert_base_trainer import MaeBertBaseTrainer
-from .maebert_model import MaeBertModel
-from .maebert_trainer import MaeBertTrainer
-from .clipbert_model import ClipBertModel
-from .clipbert_trainer import ClipBertTrainer
 from .losses import BCEFocalLoss, MultiCEFocalLoss
 from .long_seq import process_long_input
 from .model_utils import initialize_weights
